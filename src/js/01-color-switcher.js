ed65e547e60e97f64d7771f0bde300ffad33c124
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-16 15:22:33 +0200

    final1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+const body = document.querySelector('body');
+
+// let timerId = null;
+let isActive = false;
+
+startBtn.addEventListener('click', () => {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  timerId = setInterval(() => {
+    return (body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  isActive = false;
+});
