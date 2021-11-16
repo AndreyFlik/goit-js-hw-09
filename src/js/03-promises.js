2f7ca77c114a9b8fec6f8303d9d78b92cb5ef266
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-16 19:32:36 +0200

    start3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,17 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form ');
+// [(type = 'submit')];
+
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+// console.log(delayInput.name);
+
+formEl.addEventListener('click', () => {
+  console.log('Button was clicked');
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -8,3 +22,11 @@ function createPromise(position, delay) {
     console.log(delay);
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
